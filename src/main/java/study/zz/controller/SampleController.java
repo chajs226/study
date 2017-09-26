32baacbc9fb718f2dc54c1ef7c3e508465e0c47e
chajs226 <chajs226@gmail.com>
2017-09-26 14:29:18 +0900

    메시지 컨버터 껍데기까지 구현
    메시지 컨버터 껍데기까지 구현

--- src/main/java/study/zz/controller/SampleController.java
+++ src/main/java/study/zz/controller/SampleController.java
@@ -1,6 +1,7 @@
 package study.zz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,19 +11,19 @@ import study.fw.core.exception.StudyBizException;
 public class SampleController {
 
     @RequestMapping("STUDY001")
-    public @ResponseBody String retrieveSampleList() throws Exception {
+    public @ResponseBody String retrieveSampleList(@RequestBody String body) throws Exception {
 
-        String test = "A";
+        //String test = "A";
 
         System.out.println("print");
 
-        if ("A".equals(test))
+/*        if ("A".equals(test))
         {
             throw new StudyBizException();
-        }
+        }*/
 
         System.out.println("After BizException");
-        return "AA";
+        return body;
 //        throw new WrongStudyException();
 
     }
