2207297dd108ea781ff19d04deadf52a44b38deb
chajs226 <chajs226@gmail.com>
2017-10-16 17:22:42 +0900

    테스트
    -컨트롤러 requestBody STUDYData 테스트

--- src/main/java/study/zz/controller/SampleController.java
+++ src/main/java/study/zz/controller/SampleController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import study.fw.core.binding.message.STUDYData;
 import study.fw.core.exception.StudyBizException;
 
 @Controller
 public class SampleController {
 
     @RequestMapping("STUDY001")
-    public @ResponseBody String retrieveSampleList(@RequestBody String body) throws Exception {
+    public @ResponseBody STUDYData retrieveSampleList(@RequestBody STUDYData body) throws Exception {
+    //public @ResponseBody String retrieveSampleList(String body) throws Exception {
 
         //String test = "A";
 
@@ -23,6 +25,8 @@ public class SampleController {
         }*/
 
         System.out.println("After BizException");
+        
+        System.out.println(body.getHeader().toString());
         return body;
 //        throw new WrongStudyException();
 
